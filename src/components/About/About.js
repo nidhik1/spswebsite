d2d1e9db61f9f31efb17da11e79bc26f9388f89e
nidhik1 <88959345+nidhik1@users.noreply.github.com>
2023-11-29 17:06:49 -0800

    Add files via upload

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function About() {
+  return (
+    <Container fluid className="about-section">
+        <h1 className="about-heading">
+            meet the team!
+        </h1>
+    </Container>
+  );
+}
+
+export default About;
